cac9b3c94c536b268fbd9e278953e85ea1c26b81
fahimaamir <fahimaamir72@gmail.com>
2025-05-18 18:55:03 +0500

    Add files via upload

--- refpage.py
+++ refpage.py
@@ -1,7 +1,15 @@
 import streamlit as st
 from streamlit_js_eval import streamlit_js_eval
+from st_aggrid import AgGrid
+import pandas as pd
 
 st.header("Testing =======")
+df = pd.read_csv('https://raw.githubusercontent.com/fivethirtyeight/data/master/airline-safety/airline-safety.csv')
+AgGrid(df, editable = True)
+
+
+
 
 if st.button("Reload page"):
     streamlit_js_eval(js_expressions="parent.window.location.reload()")
+    
